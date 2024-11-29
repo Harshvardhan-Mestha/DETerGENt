30b8d740a3f02c4976225eefddb904e16b5ce550
Karan Bania <101618474+karannb@users.noreply.github.com>
2024-11-29 11:11:16 +0530

    evaluation push.

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ from typing import Optional
 from argparse import ArgumentParser
 
 import pandas as pd
-# from nubia import Nubia
 from src.utils.data import load_data
-from src.utils.evaluate import BERTSim
 from src.utils.config import load_config
-from src.utils.common import match, agree
 from src.lang import generate_report, generate_prediction
 from src.utils.minigpt import load_model, MiniGPTArgs, PromptArgs
 
-# NUBIA = Nubia()
 
 sys.path.append('./')
 sys.path.append('minigpt4/')
@@ -104,98 +100,14 @@ def main(config, D: list) -> Optional[pd.DataFrame]:
         expls = pd.DataFrame(expls)
         expls.to_csv(expl["out_path"])
 
-    # evaluate
-    if config["evaluate"]:
-        print("[INFO] Evaluating predictions and explanations.")
-        out_csv = evaluate_fn(list(preds["pred"]), list(expls["explanation"]), D[1], D[2])
-        return out_csv
+    # # evaluate
+    # if config["evaluate"]:
+    #     print("[INFO] Evaluating predictions and explanations.")
+    #     out_csv = evaluate_fn(list(preds["pred"]), list(expls["explanation"]), D[1], D[2])
+    #     return out_csv
 
     print("[INFO] Skipping evaluation.")
     return None
-
-
-def evaluate_fn(preds, expls, ys, es) -> pd.DataFrame:
-    """
-    Evaluates the predictions and explanations.
-
-    Args:
-        preds (List[str])
-            Predictions.
-        expls (List[str])
-            Explanations.
-        ys (List[str])
-            Ground truth labels.
-        es (List[str])
-            Ground truth explanations.
-
-    Returns:
-        pd.DataFrame
-            Dataframe with the overall evaluation.
-    """
-
-    assert len(preds) == len(expls), "[ERROR] Length of predictions and explanations do not match -- aborting evaluation."
-    c, i, po, eo = 0, 0, 0, 0
-    final_df = {
-        "Predictions": [],
-        "Explanations": [],
-        "Ground Truth": [],
-        "Ground Truth Explanation": [],
-        "predict_ok": [],
-        "explain_ok": [],
-        "bert_sim": [],
-        # "nub_score": [],
-        # "nub_semantic_rel":[],
-        # "nub_contradiction":[],
-        # "nub_irrelevancy":[],
-        # "nub_logical_agreement":[],
-        # "nub_grammar_ref":[],
-        # "nub_grammar_hyp":[]
-    }
-
-    for k, _ in enumerate(preds):
-        y_pred, y, e_pred, e = preds[k], ys[k], expls[k], es[k]
-
-        # Check if predictions and explanations are correct
-        predict_ok = match(y_pred, y)
-        explain_ok = agree(e_pred, e)
-        # nubia = NUBIA.score(ref=e,hyp=e_pred, verbose=False, get_features=True)
-        bert_sim = BERTSim(e, e_pred)
-
-        # Append to final dataframe
-        final_df["Predictions"].append(y_pred)
-        final_df["Explanations"].append(e_pred)
-        final_df["Ground Truth"].append(y)
-        final_df["Ground Truth Explanation"].append(e)
-        final_df["predict_ok"].append(predict_ok)
-        final_df["explain_ok"].append(explain_ok)
-        final_df["bert_sim"].append(bert_sim)
-        # final_df["nub_score"].append(nubia["nubia_score"])
-        # final_df["nub_semantic_rel"].append(nubia["features"]["semantic_relation"])
-        # final_df["nub_contradiction"].append(nubia["features"]["contradiction"])
-        # final_df["nub_irrelevancy"].append(nubia["features"]["irrelevancy"])
-        # final_df["nub_logical_agreement"].append(nubia["features"]["logical_agreement"])
-        # final_df["nub_grammar_ref"].append(nubia["features"]["grammar_ref"])
-        # final_df["nub_grammar_hyp"].append(nubia["features"]["grammar_hyp"])
-
-        if predict_ok and explain_ok:
-            # Correct
-            c += 1
-        elif predict_ok and not explain_ok:
-            # Prediction correct but explanation incorrect
-            po += 1
-        elif not predict_ok and explain_ok:
-            # Prediction incorrect but explanation correct
-            eo += 1
-        else:
-            # Both incorrect
-            i += 1
-
-    print(f"Correct: {c}/{len(preds)}")
-    print(f"Prediction correct but explanation incorrect: {po}/{len(preds)}")
-    print(f"Prediction incorrect but explanation correct: {eo}/{len(preds)}")
-    print(f"Both incorrect: {i}/{len(preds)}")
-
-    return pd.DataFrame(final_df)
 
 
 if __name__ == "__main__":
--- src/utils/evaluate.py
+++ src/utils/evaluate.py
@@ -3,9 +3,13 @@ Module for evaluating the model predictions.
 - BERTSim: evaluate the model predictions using the BERT for sentence similarity.
 """
 
+import pandas as pd
+# from nubia import Nubia
+from src.utils.common import match, agree
 from transformers import BertTokenizer, BertForSequenceClassification
 
 
+# NUBIA = Nubia()
 TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
 MODEL = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
@@ -38,4 +42,87 @@ def BERTSim(report_A, report_B) -> float:
     score = (cls_A @ cls_B.T).item()
 
     return score
-    
+
+
+def evaluate_fn(preds, expls, ys, es) -> pd.DataFrame:
+    """
+    Evaluates the predictions and explanations.
+
+    Args:
+        preds (List[str])
+            Predictions.
+        expls (List[str])
+            Explanations.
+        ys (List[str])
+            Ground truth labels.
+        es (List[str])
+            Ground truth explanations.
+
+    Returns:
+        pd.DataFrame
+            Dataframe with the overall evaluation.
+    """
+
+    assert len(preds) == len(expls), "[ERROR] Length of predictions and explanations do not match -- aborting evaluation."
+    c, i, po, eo = 0, 0, 0, 0
+    final_df = {
+        "Predictions": [],
+        "Explanations": [],
+        "Ground Truth": [],
+        "Ground Truth Explanation": [],
+        "predict_ok": [],
+        "explain_ok": [],
+        "bert_sim": [],
+        # "nub_score": [],
+        # "nub_semantic_rel":[],
+        # "nub_contradiction":[],
+        # "nub_irrelevancy":[],
+        # "nub_logical_agreement":[],
+        # "nub_grammar_ref":[],
+        # "nub_grammar_hyp":[]
+    }
+
+    for k, _ in enumerate(preds):
+        y_pred, y, e_pred, e = preds[k], ys[k], expls[k], es[k]
+
+        # Check if predictions and explanations are correct
+        predict_ok = match(y_pred, y)
+        explain_ok = agree(e_pred, e)
+        bert_sim = BERTSim(e, e_pred)
+        # nubia = NUBIA.score(ref=e,hyp=e_pred, verbose=False, get_features=True)
+
+        # Append to final dataframe
+        final_df["Predictions"].append(y_pred)
+        final_df["Explanations"].append(e_pred)
+        final_df["Ground Truth"].append(y)
+        final_df["Ground Truth Explanation"].append(e)
+        final_df["predict_ok"].append(predict_ok)
+        final_df["explain_ok"].append(explain_ok)
+        final_df["bert_sim"].append(bert_sim)
+        # final_df["nub_score"].append(nubia["nubia_score"])
+        # final_df["nub_semantic_rel"].append(nubia["features"]["semantic_relation"])
+        # final_df["nub_contradiction"].append(nubia["features"]["contradiction"])
+        # final_df["nub_irrelevancy"].append(nubia["features"]["irrelevancy"])
+        # final_df["nub_logical_agreement"].append(nubia["features"]["logical_agreement"])
+        # final_df["nub_grammar_ref"].append(nubia["features"]["grammar_ref"])
+        # final_df["nub_grammar_hyp"].append(nubia["features"]["grammar_hyp"])
+
+        if predict_ok and explain_ok:
+            # Correct
+            c += 1
+        elif predict_ok and not explain_ok:
+            # Prediction correct but explanation incorrect
+            po += 1
+        elif not predict_ok and explain_ok:
+            # Prediction incorrect but explanation correct
+            eo += 1
+        else:
+            # Both incorrect
+            i += 1
+
+    print(f"Correct: {c}/{len(preds)}")
+    print(f"Prediction correct but explanation incorrect: {po}/{len(preds)}")
+    print(f"Prediction incorrect but explanation correct: {eo}/{len(preds)}")
+    print(f"Both incorrect: {i}/{len(preds)}")
+
+    return pd.DataFrame(final_df)
